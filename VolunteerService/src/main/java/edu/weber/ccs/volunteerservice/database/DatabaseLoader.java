695d5d49f148e610b8b93165e2db964e5c637672
ItzAblur <jakeabelur@gmail.com>
2015-07-11 14:38:15 -0600

    init commit

---
+++ VolunteerService/src/main/java/edu/weber/ccs/volunteerservice/database/DatabaseLoader.java
@@ -0,0 +1,23 @@
+package edu.weber.ccs.volunteerservice.database;
+
+/**
+ * Created by Jake on 7/11/2015.
+ */
+public class DatabaseLoader {
+
+
+    public DatabaseLoader() {
+
+        System.out.println("Loading database");
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+        new DatabaseLoader();
+    }
+
+
+}
