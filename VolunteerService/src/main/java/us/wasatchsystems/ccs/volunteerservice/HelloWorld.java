b950d55c56b10516ace6c691fae7bcfb97c3bc0c
ItzAblur <jakeabelur@gmail.com>
2015-07-12 11:19:43 -0600

    Initial commit, setup the whole project and made it ready for deployment.

---
+++ VolunteerService/src/main/java/us/wasatchsystems/ccs/volunteerservice/HelloWorld.java
@@ -0,0 +1,11 @@
+package us.wasatchsystems.ccs.volunteerservice;
+
+/**
+ * Created by Jake on 7/12/2015.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
